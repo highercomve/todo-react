9cf3ef8d301d876fa82d0ffbbfb6d34d1a18edbc
Sergio Marin <higher.vnf@gmail.com>
2015-09-17 17:37:36 -0300

    Some Styles

--- components/task_item.js
+++ components/task_item.js
@@ -4,17 +4,17 @@ import TodoStore from '../stores/todo_store'
 
 
 class TaskItem extends React.Component {
-  
+
   constructor() {
     super()
     this.state = { title: '', id: 1, status: '' }
   }
 
   componentDidMount() {
-    this.setState({ 
-      title: this.props.task.title, 
-      id: this.props.task.id, 
-      status: this.props.task.status 
+    this.setState({
+      title: this.props.task.title,
+      id: this.props.task.id,
+      status: this.props.task.status
     })
   }
 
@@ -22,12 +22,12 @@ class TaskItem extends React.Component {
     console.info('Removing task with index: ', this.state)
     TodoStore.remove(this.state)
   }
-  
-  markDone(e) {     
+
+  markDone(e) {
     //TodoStore.toggleStatus(this.state)
     this.setState(TodoStore.toggleStatus(this.state))
   }
-  
+
   render() {
     var checkDone = (this.state.status === TodoStore.DONE ) ? true : false
     var toggleForID = `statusToggle-${this.state.id}`
@@ -41,14 +41,14 @@ class TaskItem extends React.Component {
         <input
           id={toggleForID}
           className="task-status-toggle"
-          type="checkbox"  
+          type="checkbox"
           checked={checkDone}
           onChange={this.markDone.bind(this)} />
         <label htmlFor={toggleForID}></label>
         <span className="task-title">{ this.state.title }</span>
-        <button className="task-remove" onClick={this.removeTask.bind(this)} >delete</button>
+        <button className="task-remove" onClick={this.removeTask.bind(this)} >&times;</button>
       </li>
-    )  
+    )
   }
 
 }
